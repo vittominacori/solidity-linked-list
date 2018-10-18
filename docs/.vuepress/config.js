c4a64b39ce87430d9c566250209ff4e21d45b1bf
vittominacori <vittoriominacori@gmail.com>
2018-10-18 17:36:07 +0200

    adding ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Solidity Linked List',
   description: 'An utility library for using sorted linked list data structures in your Solidity project',
   base: '/solidity-linked-list/',
+  ga: 'UA-115756440-2',
   themeConfig: {
     sidebar: 'auto',
   },
