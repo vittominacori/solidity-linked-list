df558647f7e2910d245030fb89b9a125d51bb5fc
Vittorio Minacori <vittoriominacori@gmail.com>
2024-02-27 10:30:22 +0100

    chore: update dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   compilers: {
     solc: {
-      version: '0.8.22',
+      version: '0.8.24',
       settings: {
+        evmVersion: 'shanghai',
         optimizer: {
           enabled: true,
           runs: 200,
